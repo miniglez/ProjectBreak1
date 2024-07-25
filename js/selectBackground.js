584c53d71630e499d080d62550ee9e5c912ac41a
Ivan <miniglez.98@gmail.com>
2024-07-25 23:12:55 +0200

    Bug fix

--- js/selectBackground.js
+++ js/selectBackground.js
@@ -1,8 +1,14 @@
 const body = document.body
-console.log(body)
-let count = 1
+const title = document.title
+console.log(title)
+let count = Math.floor((Math.random()*10)+1)
 const selectBackground = () => {
-    document.body.style.backgroundImage = "url(../assets/background/background" + count + ".jpg)"
+    if(title == "Dashboard"){
+        document.body.style.backgroundImage = "url(./assets/background/background" + count + ".jpg)"
+    }
+    else{
+        document.body.style.backgroundImage = "url(../assets/background/background" + count + ".jpg)"
+    }
     count ++
     if (count > 10){
         count = 1
